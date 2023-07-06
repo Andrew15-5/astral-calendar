008f760db8a040789419f1a9ae0dcaf889a3f11c
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-07-06 18:43:56 +0300

    Added 3 calendars with correct month-year-text

--- src/server/calendar.ts
+++ src/server/calendar.ts
@@ -55,6 +55,11 @@ function get_selectors_data() {
   }))
 }
 
+type CalendarData = {
+  'month-year-text': string
+  'cell-matrix': number[][]
+}
+
 namespace calendar {
   export function get_main(_request: Request, response: Response) {
     response.status(200).render('main', { selectors: get_selectors_data() })
@@ -68,11 +73,32 @@ namespace calendar {
     response.status(200).render('month', { year, month: month_str })
   }
   export function get_quater(
-    request: Request<{ year: string; quater: string }>,
+    request: Request<{ year: string; quater: number }>,
     response: Response
   ) {
     const { year, quater } = request.params
-    response.status(200).render('quater', { year, quater })
+    const cells = Array.from({ length: 35 }, (_, i) => (i % 31) + 1)
+    const matrix = []
+    const row_length = 7
+    while (cells.length > 0) {
+      matrix.push(cells.splice(0, row_length))
+    }
+    const first_month_index = (quater - 1) * 3
+    const last_month_index = first_month_index + 2
+    const quater_month_names = []
+    for (let i = first_month_index; i <= last_month_index; i++) {
+      quater_month_names.push(month_names[i])
+    }
+    const calendars_data: CalendarData[] = []
+    for (const month_name of quater_month_names) {
+      calendars_data.push({
+        'month-year-text': `${month_name} ${year}`,
+        'cell-matrix': matrix,
+      })
+    }
+    response
+      .status(200)
+      .render('quater', { year, quater, calendars: calendars_data })
   }
 }
 
